4c79bda5068018c15c598144c28910cc7c743ea7
carson-jpg <isavameshack@gmail.com>
2025-08-30 22:04:33 +0800

    is impleemenet

--- server/middleware/roleCheck.js
+++ server/middleware/roleCheck.js
@@ -1,4 +1,4 @@
-const checkRole = (roles) => {
+const checkRole = (...roles) => {
   return (req, res, next) => {
     if (!req.user) {
       return res.status(401).json({ message: 'Authentication required' });
